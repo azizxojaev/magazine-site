b09beb03a78f9c14bb1abffeec68341710e606b4
azizxojaev <azizxojaevbexruz@gmail.com>
2024-02-14 21:50:59 +0500

    fixed slug 3

--- news/models.py
+++ news/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 from ckeditor.fields import RichTextField
-from .utils import custom_slugify
+import re
 
 
 class Tag(models.Model):
@@ -37,7 +37,8 @@ class New(models.Model):
     date = models.DateField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
-        self.slug = custom_slugify(self.title)
+        value = re.sub(r'[^\w\s-]', '', self.title).strip().lower()
+        self.slug = re.sub(r'[-\s]+', '-', value)
         super(New, self).save(*args, **kwargs)
 
     def __str__(self):
